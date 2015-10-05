ef04cfc92b96ee66039b2c88b32f2bc3029f9e94
somkiat <somkiat.p@gmail.com>
2015-10-05 20:12:57 +0700

    Initial Commit

---
+++ DemoQuoteTests/DemoQuoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  DemoQuoteTests.swift
+//  DemoQuoteTests
+//
+//  Created by somkiat puisungnoen on 10/5/2558 BE.
+//  Copyright © 2558 ___UP1___. All rights reserved.
+//
+
+import XCTest
+@testable import DemoQuote
+
+class DemoQuoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
